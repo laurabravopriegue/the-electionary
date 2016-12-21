b490b2820e5a0c1340378e303f4df9aea0119226
keelanfh <info@keelanfh.com>
2016-12-21 18:50:32 +0000

    Update transcript-downloader.py

--- debateScrape/debateScrape/spiders/transcript-downloader.py
+++ debateScrape/debateScrape/spiders/transcript-downloader.py
@@ -8,18 +8,26 @@ import json
 import os
 
 class MySpider(BaseSpider):
+    # Name the spider as required by scrapy
     name = "download"
+    
+    # Set a download delay of 5 seconds.
+    # This makes the downloads take longer, but it's kinder on the UCSB servers.
     download_delay = 5
 
+    # Import the list of URLs to download from - this comes from our earlier file.
     with file('urls.json', 'r') as f:
-        startUrls = json.load(f)
-
+        start_urls = json.load(f)
+    
+    # Once again, the allowed domain is set.
     allowed_domains = ['ucsb.edu']
-    start_urls = startUrls
-
+    
+    # parse method, run every time the spider is run
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-
+        
+        # The only purpose of the following code is to give the files sensible names.
+        
         # Extract the date of the debate
         date = hxs.select("//span[@class='docdate']/text()").extract()
         for date in date:
@@ -33,6 +41,8 @@ class MySpider(BaseSpider):
             break
 
         fileName = debateDate + " " + debateName + ".html"
-
+        
+        
+        # The files are then saved to a directory, for access later.
         with open(os.path.join('html-files', fileName), 'w') as f:
             f.write(response.body)
