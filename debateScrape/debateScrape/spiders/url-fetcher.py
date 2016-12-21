5c41543067154531cb4d5ce0368c9ef6fa89e4e6
keelanfh <info@keelanfh.com>
2016-12-21 18:45:15 +0000

    Update url-fetcher.py

--- debateScrape/debateScrape/spiders/url-fetcher.py
+++ debateScrape/debateScrape/spiders/url-fetcher.py
@@ -1,25 +1,41 @@
 # URL Fetcher
-# Fetches a list of URLs to be used by Transcript Processor
+# Fetches a list of URLs to be used by transcript-downloader.py
+
+# Import BaseSpider and HtmlXPathSelector from scrapy
+# Also import JSON so we can export the file for later use
 
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 import json
 
-
+# MySpider instantiates BaseSpider
 class MySpider(BaseSpider):
+    
+    # The spider must be named so we can call it later from Terminal
     name = "urlfetch"
-
-    url = "http://www.presidency.ucsb.edu/debates.php"
-
+    
+    # We tell scrapy where to start the crawl, and which domains are allowed
     allowed_domains = ["ucsb.edu"]
-    start_urls = [url]
-
+    start_urls = ["http://www.presidency.ucsb.edu/debates.php"]
+    
+    # response is something that scrapy generates when the spider is run
+    # scrapy will automatically call this method when the spider is run
     def parse(self, response):
-
+        
+        # HtmlXPathSelector is instantiated with the response
         hxs = HtmlXPathSelector(response)
-
+        
+        # The relevant urls are extracted from the HTML response
+        # The string is an XPath statement
+        # //td[@class='doctext'] means select all <td> elements in the HTML that have class attribute equal to 'doctext'
+        # This selects elements like <td class='doctext'/>
+        # In this case, there is only one such element
+        # /a/@href means select the href attribute from any <a> element in this
+        # e.g. <a href='www.example.com'>
+        # .extract() then extracts the text element from this, and returns a list
         urls = hxs.select("//td[@class='doctext']/a/@href").extract()
-
+        
+        # Create a dictionary which has the urls included in it.
         dictionary = {"allowed_domains": self.allowed_domains, "start_urls": urls}
 
         with open('urls.json', 'w') as f:
